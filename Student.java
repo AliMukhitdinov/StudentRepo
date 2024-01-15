0b9d7140546f5cd018811a1bcf6d90ce0f561045
Ali Mukhitdinov <mukhitdinov.a.433@gmail.com>
2024-01-15 16:56:49 +0500

    First Classes created

---
+++ Student.java
@@ -0,0 +1,55 @@
+public class Student {
+    private String name;
+    private Integer age;
+    private String address;
+    private String phoneNumber;
+
+    public Student(String name, Integer age, String address, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
